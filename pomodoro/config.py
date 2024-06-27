ca559d68aba078ed9a935687637a3cbed6d15048
BuildTools <unconfigured@null.spigotmc.org>
2024-06-27 10:56:29 +0400

    config

--- pomodoro/config.py
+++ pomodoro/config.py
@@ -1,19 +1,7 @@
+# config.py
 
-import os
-import hashlib
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 WORK_TIME_MIN = 25
 BREAK_TIME_MIN = 5
 BEEP_FREQUENCY = 440
-BEEP_DURATION = 0.5
+BEEP_DURATION = 0.3
 BEEP_VOLUME = 0.5
-
-def get_beep_filename():
-    if not os.path.exists(os.path.join(BASE_DIR, 'assets')):
-        os.makedirs(os.path.join(BASE_DIR, 'assets'))
-    params = f"{BEEP_FREQUENCY}_{BEEP_DURATION}_{BEEP_VOLUME}"
-    params_hash = hashlib.md5(params.encode()).hexdigest()
-    return os.path.join(BASE_DIR, 'assets', f'beep_{params_hash}.wav')
-
-BEEP_FILENAME = get_beep_filename()
