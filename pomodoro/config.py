e524fabd30f14e244fcdd0db8f315ef1b76fc56d
BuildTools <unconfigured@null.spigotmc.org>
2024-06-27 09:41:10 +0400

    assets dir added

--- pomodoro/config.py
+++ pomodoro/config.py
@@ -10,6 +10,8 @@ BEEP_DURATION = 0.5
 BEEP_VOLUME = 0.5
 
 def get_beep_filename():
+    if not os.path.exists(os.path.join(BASE_DIR, 'assets')):
+        os.makedirs(os.path.join(BASE_DIR, 'assets'))
     params = f"{BEEP_FREQUENCY}_{BEEP_DURATION}_{BEEP_VOLUME}"
     params_hash = hashlib.md5(params.encode()).hexdigest()
     return os.path.join(BASE_DIR, 'assets', f'beep_{params_hash}.wav')
